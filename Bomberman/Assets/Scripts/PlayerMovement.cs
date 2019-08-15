eaf91a6bd937fc58acfb6f44f86f854494264ba7
Noxalus <noxalus@gmail.com>
2019-08-15 19:36:58 +0200

    Use blend tree to handle player animation according to its movement.

--- Bomberman/Assets/Scripts/PlayerMovement.cs
+++ Bomberman/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private Rigidbody2D _rigidbody = null;
+    [SerializeField] private Animator _animator = null;
 
     private Vector2 _movement = Vector2.zero;
 
@@ -13,6 +12,10 @@ public class PlayerMovement : MonoBehaviour
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
+
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
     }
 
     void FixedUpdate()
