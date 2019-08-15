107476849ff9c64a746280a4cc5295a413a2c7f9
Noxalus <noxalus@gmail.com>
2019-08-15 19:14:25 +0200

    Create a script to move the player.

---
+++ Bomberman/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D _rigidbody = null;
+
+    private Vector2 _movement = Vector2.zero;
+
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _movement * _moveSpeed * Time.fixedDeltaTime);
+    }
+}
