4a4753c354f1f97a035c974c2283ccd3c673600d
Noxalus <noxalus@gmail.com>
2019-09-19 23:40:46 +0200

    Spawn multiple players using the spawn points.

--- Bomberman/Assets/Scripts/GameManager.cs
+++ Bomberman/Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Scene reference")]
+
     [SerializeField] private WallGenerator _wallGenerator = null;
 
+    [Header("Assets reference")]
+
+    [SerializeField] private Player _playerPrefab = null;
+
     private Map _map = null;
+    private List<Player> _players = new List<Player>();
 
     private void Start()
     {
@@ -34,6 +42,17 @@ public class GameManager : MonoBehaviour
         {
             _wallGenerator.Initialize(_map);
             _wallGenerator.GenerateWalls(1f);
+
+            StartGame(4);
+        }
+    }
+
+    private void StartGame(int playerCount = 1)
+    {
+        for (int i = 0; i < playerCount; i++)
+        {
+            var player = Instantiate(_playerPrefab, _map.GetSpawnPosition(i), Quaternion.identity);
+            _players.Add(player);
         }
     }
 }
